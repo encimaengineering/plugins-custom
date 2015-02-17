714e5f9ca107ddc1ad95312b9aad2d42cd901bcd
sfainstein-encima <sfainstein@Scotts-Encima-MacBook-Pro.local>
2015-02-17 16:53:47 -0500

    updated with note

---
+++ script-pagename.js
@@ -0,0 +1,21 @@
+/*  Plugin - site section  */
+//some change
+function cleanUp (value) {
+  return value.toLowerCase().replace("-"," ").replace(".aspx","");
+}
+
+var parts = window.location.href.split("/"),
+    total = parts.length,
+    count = 0,
+    siteSection = "unknown",
+    s_pageName = "",
+    urlPath = document.URL.toLowerCase();
+
+if(urlPath.indexOf('.aspx') > -1){
+    s_pageName = parts[3]
+}else{
+    s_pageName = parts[4] + '|' + parts[3];
+}
+
+s_pageName = cleanUp(s_pageName);
+console.log('s_pageName = ' + s_pageName);
