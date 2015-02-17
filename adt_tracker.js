087aa7c8946cbd3c249b336c92d2dc7b3275efae
sfainstein-encima <sfainstein@Scotts-Encima-MacBook-Pro.local>
2015-02-17 17:40:10 -0500

    test 3

--- adt_tracker.js
+++ adt_tracker.js
@@ -1,4 +1,3 @@
-//test 25-444
 (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
